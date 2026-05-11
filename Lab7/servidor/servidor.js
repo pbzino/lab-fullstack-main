a18c61a4fb6cdb075d94baa64a55c2c19cdfdf50
pbzino <pedrobarreto.oliveira08@gmail.com>
2026-05-11 17:03:11 -0300

    formatando as pastas

---
+++ Lab7/servidor/servidor.js
@@ -0,0 +1,8 @@
+require("colors");
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('./public'));
+var server = http.createServer(app);
+server.listen(80);
+console.log("servidor rodando...".blue)
