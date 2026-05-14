86c8e1011db94da06463e22f0fb6dbc32410772f
pbzino <pedrobarreto.oliveira08@gmail.com>
2026-05-14 17:33:49 -0300

    lab 9

--- Lab9/servidor.js
+++ Lab9/servidor.js
@@ -1,14 +1,45 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const dns = require("dns");
 
-const app = express();
+dns.setServers([
+    "8.8.8.8",
+    "1.1.1.1"
+]);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var http = require("http");
+var express = require("express");
+var bodyParser = require("body-parser")
+var path = require("path");
+var mongodb = require("mongodb");
 
-app.use(express.static(path.join(__dirname, "public")));
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb+srv://PedroBarreto:w41rnDFrAarA8kPw@pedro-db.jxvlhje.mongodb.net/?appName=pedro-db';
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-app.set("view engine", "ejs");
+var posts;
+var usuarios;
+var carros;
+
+client.connect(function(err) {
+    if (err) {
+        console.log("Erro ao conectar no banco: " + err);
+    } else {
+        console.log("Conectado ao MongoDB!");
+        var dbo = client.db("blog_bd");
+        posts    = dbo.collection("posts");
+        usuarios = dbo.collection("usuarios"); 
+        carros   = dbo.collection("carros");   
+    }
+});
+
+
+var app = express();
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+
 
 let usuarioSalvo = "";
 let senhaSalva = "";
@@ -57,6 +88,38 @@ app.post("/logar", (req, res) => {
 
 });
 
-app.listen(80, () => {
-    console.log("Servidor rodando");
+app.get('/blog', function(req, res) {
+    posts.find({}).toArray(function(err, items) {
+        if (err) {
+            console.log(err);
+            res.send("Erro ao buscar posts!");
+        } else {
+            res.render('blog', { listaPosts: items });
+        }
+    });
 });
+
+app.get('/cadastrar_post', function(req, res) {
+  res.sendFile(__dirname + '/public/cadastrar_post.html');
+});
+ 
+app.post('/salvar_post', function(req, res) {
+    var data = {
+        titulo:   req.body.titulo,
+        resumo:   req.body.resumo,
+        conteudo: req.body.conteudo
+    };
+    posts.insertOne(data, function(err) {
+        if (err) {
+            console.log(err);
+            res.send("Erro ao salvar post!");
+        } else {
+            res.redirect('/blog');
+        }
+    });
+});
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log("Servidor rodando ...");
